2c8e4b26e26e1f6b4b0ed09f9720132e6ebd4f42
Reza <RezaTaghipour99@gmail.com>
2023-05-07 00:39:28 +0430

    directive completed

--- src/app/answer-highlight.directive.ts
+++ src/app/answer-highlight.directive.ts
@@ -1,17 +1,36 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
 import { NgControl } from '@angular/forms';
+import { Subscription, map } from 'rxjs';
 
 @Directive({
   selector: '[appAnswerHighlight]'
 })
 export class AnswerHighlightDirective implements OnInit{
 
+  sub: Subscription | undefined;
   constructor(private elRef: ElementRef, private controlName: NgControl) { 
   }
 
   ngOnInit(): void {
     // console.log(this.elRef.nativeElement);
-    console.log(this.controlName);
+    //console.log(this.controlName);
+    this.sub = this.controlName.control?.parent?.valueChanges
+    .pipe(map(({a,b, answer})=>{
+        return Math.abs((a+b-answer)/(a+b));
+      }))
+    .subscribe( val => {
+      if(val < 0.2)
+        this.elRef.nativeElement.classList.add('highlight');
+      else
+      this.elRef.nativeElement.classList.remove('highlight');
+
+    })
   }
+
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
+  
 
 }
